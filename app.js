0f0eef45b99ad0153bea6852ec0c53569f871d11
ffon <poty1900@hotmail.com>
2018-03-19 17:29:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var data = require('./respond.json');
-var question = require('./question.json');
+// var data = require('./respond.json');
+// var question = require('./question.json');
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function(){
@@ -15,38 +15,35 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 
-
-var timeout = undefined;
-
 var msg = server.post('api/messages', connector.listen());
 
 
 bot.dialog('/', function (session) {
     
     var req = session.message.text;
-    var resKey = null;
-    var keys = Object.keys(data);
-    for(var i=0; i<keys.length; i++){
-        var key = keys[i];
-        var regex = new RegExp(key);
-        if(req.match(regex)){
-            resKey = key;            
-            break;
-        }
+//     var resKey = null;
+//     var keys = Object.keys(data);
+//     for(var i=0; i<keys.length; i++){
+//         var key = keys[i];
+//         var regex = new RegExp(key);
+//         if(req.match(regex)){
+//             resKey = key;            
+//             break;
+//         }
         
-    }
-    if(resKey){
-        var s = 'นี้จ้า'+"\n";
-        session.send(s+data[resKey]);
+//     }
+//     if(resKey){
+//         var s = 'นี้จ้า'+"\n";
+//         session.send(s+data[resKey]);
         
-    } else {
+//     } else {
         
-        var res = 'สวัสดีจ้าา เราคือบอท KunSri'+'\n';
-        question.forEach(function(questions,index){
-            res += "\n"+questions;
+//         var res = 'สวัสดีจ้าา เราคือบอท KunSri'+'\n';
+//         question.forEach(function(questions,index){
+//             res += "\n"+questions;
             
-        });
-        session.send(res);
+//         });
+        session.send("Hello u");
     }           
            
 });
