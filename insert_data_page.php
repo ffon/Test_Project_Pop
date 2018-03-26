752fe3ece0d0a35a63a06e890a3f6681d5923615
ffon <poty1900@hotmail.com>
2018-03-26 22:46:18 +0700

    Update insert_data_page.php

--- insert_data_page.php
+++ insert_data_page.php
@@ -1,9 +1,9 @@
 <?php
- $arrne["key"] = "dsds";
+ $arrne = "dsds";
 $inp = file_get_contents('respond.json');
 // $tempArray = json_decode($inp); // ข้อมูลใน json
 
 $tempArray = json_decode($inp, true);
-array_push($tempArray,  $arrne);
+array_push($tempArray["key"],  $arrne);
 
  var_dump($tempArray);
