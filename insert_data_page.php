d8c94e9acef13ec209a8c9c3e96528389460235f
ffon <poty1900@hotmail.com>
2018-03-26 22:47:40 +0700

    Update insert_data_page.php

--- insert_data_page.php
+++ insert_data_page.php
@@ -4,6 +4,7 @@ $inp = file_get_contents('respond.json');
 // $tempArray = json_decode($inp); // ข้อมูลใน json
 
 $tempArray = json_decode($inp, true);
-array_push($tempArray["key"],  $arrne);
+// array_push($tempArray["key"],$arrne);
+array_push($tempArray["key"] , $arrne);
 
  var_dump($tempArray);
