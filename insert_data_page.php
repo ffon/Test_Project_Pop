c2b85cb7861cfb9cd75bcf6adcbae680fea9d93b
ffon <poty1900@hotmail.com>
2018-03-27 00:39:57 +0700

    Update insert_data_page.php

--- insert_data_page.php
+++ insert_data_page.php
@@ -11,7 +11,7 @@
 ?>
 <!DOCTYPE html>  
  <html>  
-    <head>  0
+    <head>  11
 	<title>Save to JSON file using PHP</title>  
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
@@ -55,6 +55,7 @@
 		    d3.json("respond.json", function(error, data) {
 		    if (error)
 		      throw error;
+		    data["คำร้องทั่วไป"] = "123";
 		    console.log(data["คำร้องทั่วไป"]);
 		  });
 		    	
