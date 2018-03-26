887149e4c094cbff743e366ac4897b454e098266
ffon <poty1900@hotmail.com>
2018-03-26 22:55:05 +0700

    Update insert_data_page.php

--- insert_data_page.php
+++ insert_data_page.php
@@ -1,8 +1,22 @@
-<?php
-$arrne = "dsds";
-$inp = file_get_contents('respond.json');
+// <?php
+// $arrne = "dsds";
+// $inp = file_get_contents('respond.json');
 
-$tempArray = json_decode($inp, true);
-array_push($tempArray["key"],$arrne);
+// $tempArray = json_decode($inp, true);
+// array_push($tempArray["key"],$arrne);
 
- var_dump($tempArray);
+//  var_dump($tempArray);
+
+
+$str = file_get_contents('respond.json');
+$arr = json_decode($str, true);
+$arrne['roll_over_image'] = "sadaadad";
+array_push( $arr['employees'], $arrne);
+$str = json_encode($arr);
+if (json_decode($str) != null)
+ {
+   $file = fopen('respond.json','w');
+   fwrite($file, $str);
+   fclose($file);
+ }
+
