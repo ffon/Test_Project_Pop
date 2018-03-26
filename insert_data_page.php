62a8dd58e108324baad6012eb6a17f113085e91c
ffon <poty1900@hotmail.com>
2018-03-26 21:40:51 +0700

    1

--- insert_data_page.php
+++ insert_data_page.php
@@ -1,10 +1,22 @@
 <?php
 
-// $data[] = $_POST['data'];
+$data[] = array("test" => "data1");
+echo "data \n";
+var_dump($data);
+echo "-------------------------------------\n";
 
 $inp = file_get_contents('respond.json');
-$tempArray = json_decode($inp);
+$tempArray = json_decode($inp); // ข้อมูลใน json
+echo "tempArray be \n";
 var_dump($tempArray);
-// array_push($tempArray, $data);
-// $jsonData = json_encode($tempArray);
+
+echo "-------------------------------------\n";
+array_push($tempArray, $data);
+echo "tempArray after \n";
+var_dump($tempArray);
+echo "-------------------------------------\n";
+$jsonData = json_encode($tempArray);
+echo "jsonData \n";
+var_dump($jsonData);
+echo "-------------------------------------\n";
 // file_put_contents('respond.json', $jsonData);
