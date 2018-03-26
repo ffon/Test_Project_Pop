53345eb4c23d712e70c911bce036374e5f93cae9
ffon <poty1900@hotmail.com>
2018-03-26 21:20:29 +0700

    ok

--- insert_data_page.php
+++ insert_data_page.php
@@ -1,2 +1,11 @@
 <?php
-echo "yes this page !!"; 
+
+// $data[] = $_POST['data'];
+
+$inp = file_get_contents('respond.json');
+$tempArray = json_decode($inp);
+
+var_dump($tempArray);
+// array_push($tempArray, $data);
+// $jsonData = json_encode($tempArray);
+// file_put_contents('respond.json', $jsonData);
