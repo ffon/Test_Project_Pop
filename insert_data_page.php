4882aca8f8b917e618a45d39f50e4cc25779e446
ffon <poty1900@hotmail.com>
2018-03-27 00:33:54 +0700

    Update insert_data_page.php

--- insert_data_page.php
+++ insert_data_page.php
@@ -11,7 +11,7 @@
 ?>
 <!DOCTYPE html>  
  <html>  
-    <head>  8
+    <head>  0
 	<title>Save to JSON file using PHP</title>  
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
@@ -55,8 +55,7 @@
 		    d3.json("respond.json", function(error, data) {
 		    if (error)
 		      throw error;
-		    var jsonData = JSON.parse(data);
-		    console.log(jsonData);
+		    console.log(data["คำร้องทั่วไป"]);
 		  });
 		    	
 	    </script>
