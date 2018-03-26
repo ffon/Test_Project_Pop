8b65b7d16eb916c487cc715d825b53cf38dd1782
ffon <poty1900@hotmail.com>
2018-03-26 23:23:20 +0700

    Update insert_data_page.php

--- insert_data_page.php
+++ insert_data_page.php
@@ -3,15 +3,53 @@
 
 // $str = file_get_contents('respond.json');
 // $arr = json_decode($str, true);
-array_push( $arr['employees'], "sadaadad");
+// array_push( $arr['employees'], "sadaadad");
 
-var_dump($arr);
+// var_dump($arr);
 
-if (count($arr)>0)
- {
-   var_dump($arr);
-   $file = fopen('respond.json','a+');
-   fwrite($file, $arr);
-   fclose($file);
- }
+// if (count($arr)>0)
+//  {
+//    var_dump($arr);
+//    $file = fopen('respond.json','a+');
+//    fwrite($file, $arr);
+//    fclose($file);
+//  }
+
+
+	$error = '';
+	if(isset($_POST["submit"]))
+	{
+		if(empty($_POST["name"]))
+		{
+			$error = "<label class='text-danger'>( Enter a name please. )</label>";
+		}
+		else if(empty($_POST["email"]))
+		{
+			$error = "<label class='text-danger'>( Enter a email address please. ) </label>";
+		}
+		else
+		{
+			if(file_exists('data.json'))
+			{
+				$current_data = file_get_contents('data.json');
+				$array_data = json_decode($current_data, true);
+				$form_data = array(
+					'name' => $_POST['name'],
+					'email' => $_POST['email']
+				);
+				$array_data[] = $form_data;
+				$data_proccesed = json_encode($array_data, JSON_PRETTY_PRINT);
+				if(file_put_contents('data.json', $data_proccesed))
+				{
+					header("Location: index.php");
+					exit();
+				}
+			}
+			else
+			{
+				$error = "<label class='text-danger'>File not found!</label>";
+			}
+		}
+	}
+	?>
 
