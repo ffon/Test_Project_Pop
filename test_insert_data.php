49488a75d97d60e5ab794b992129ffab32405783
ffon <poty1900@hotmail.com>
2018-03-30 19:13:19 +0700

    Update test_insert_data.php

--- test_insert_data.php
+++ test_insert_data.php
@@ -1,5 +1,5 @@
 <html>  
-    <head>  test
+    <head>  test1
 	<title>Save to Data file using PHP</title>  
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
@@ -7,6 +7,8 @@
     <body>  
 <script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
       <script>
+	      
+	      console.log("yes");
         // Initialize Firebase
         var config = {
           apiKey: "AIzaSyDd1JiN7FNLo9KvG9OCe1Mrn-ZB_9_bNFE",
