bea9658371e01ef4a40d9e302cc0551c19783c23
ffon <poty1900@hotmail.com>
2018-03-30 18:35:37 +0700

    Create test_insert_data.php

---
+++ test_insert_data.php
@@ -0,0 +1,23 @@
+<html>  
+    <head>  1
+	<title>Save to Data file using PHP</title>  
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script></head>  
+    <body>  
+	    <script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
+      <script>
+        // Initialize Firebase
+        var config = {
+          apiKey: "AIzaSyDd1JiN7FNLo9KvG9OCe1Mrn-ZB_9_bNFE",
+          authDomain: "ksbot-ecbd2.firebaseapp.com",
+          databaseURL: "https://ksbot-ecbd2.firebaseio.com",
+          projectId: "ksbot-ecbd2",
+          storageBucket: "ksbot-ecbd2.appspot.com",
+          messagingSenderId: "421898172154"
+        };
+        firebase.initializeApp(config);
+      </script>
+	
+    </body>  
+ </html> 
