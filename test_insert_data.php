982fe2f4e171f9c8ac8acb48f0b42d67211b3a95
ffon <poty1900@hotmail.com>
2018-03-30 20:07:12 +0700

    Update test_insert_data.php

--- test_insert_data.php
+++ test_insert_data.php
@@ -1,5 +1,5 @@
 <html>  
-    <head>  test2
+    <head>  test
 	<title>Save to Data file using PHP</title>  
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
@@ -21,25 +21,38 @@
         firebase.initializeApp(config);
 	      
 	  // ------  
-
-	      
-	var database = firebase.database();
-	var leadsRef = database.ref('ksbot-ecbd2');
-	var newClientKey = database.ref().child('ksbot-ecbd2').push().key;
-	console.log(newClientKey);
-	var childData;
-	var childKey;
-	leadsRef.on('value', function(snapshot) {
-		console.log("YYY");
-		
+	var userDataRef = firebase.database().ref("ksbot-ecbd2").orderByKey();
+	userDataRef.once("value")
+	  .then(function(snapshot) {
 	    snapshot.forEach(function(childSnapshot) {
-	      	childKey  = childSnapshot.key;
-    		childData = childSnapshot.val();
-		console.log(childData);
-		console.log('---------------');
-	    });
-		console.log(childData);
-	});
+	      var key = childSnapshot.key;
+	      var childData = childSnapshot.val();              // childData will be the actual contents of the child
+
+	      var name_val = childSnapshot.val().Name;
+	      var id_val = childSnapshot.val().AssignedID;
+	      console.log(name_val);
+	      console.log(id_val);
+	  });
+	 });
+	}());
+	      
+// 	var database = firebase.database();
+// 	var leadsRef = database.ref('ksbot-ecbd2');
+// 	var newClientKey = database.ref().child('ksbot-ecbd2').push().key;
+// 	console.log(newClientKey);
+// 	var childData;
+// 	var childKey;
+// 	leadsRef.on('value', function(snapshot) {
+// 		console.log("YYY");
+		
+// 	    snapshot.forEach(function(childSnapshot) {
+// 	      	childKey  = childSnapshot.key;
+//     		childData = childSnapshot.val();
+// 		console.log(childData);
+// 		console.log('---------------');
+// 	    });
+// 		console.log(childData);
+// 	});
 	
 	      
 	      
