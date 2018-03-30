04bdf65d3469dde8cc439b4ec8937d3316005a9c
ffon <poty1900@hotmail.com>
2018-03-31 00:50:21 +0700

    Update test_insert_data.php

--- test_insert_data.php
+++ test_insert_data.php
@@ -2,6 +2,7 @@
 <head> 
 <title>Data management</title>  
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src = "https://cdn.firebase.com/js/client/2.4.2/firebase.js"></script>
@@ -22,6 +23,7 @@
       color: white;   
   }
   
+
 </style>
 </head>  
 	<body> 
@@ -56,7 +58,7 @@
   
 
 
-    <div class="grid-result-data">
+    <div class="grid-result-data" >
       <div class="table-responsive-report" style="padding: 70px 15px 10px 15px;">
         <table class="clickable table table-hover table-striped sticky-enabled tableheader-processed sticky-table" id="table-vehicle">
             <thead>
@@ -76,7 +78,7 @@
                 <td class="key">test_key</td>
                 <td class="value">test_value</td>
                 <td class="created">Created</td>
-                <td class="operations">Operations</td>
+                <td class="operations"<i class="material-icons" style="font-size:48px;color:red">delete</i> &nbsp </td>
               </tr>
 
             </tbody>
@@ -105,13 +107,13 @@
 
       // var push_Ref = ref("key3").set("value"); // เพิ่มข้อมูลลงใน file base
 
-      ref.on("value", function(snapshot) { // อ่านค่าจากจาก filebase
-        console.log('y');
-        console.log(snapshot.val());
+      // ref.on("value", function(snapshot) { // อ่านค่าจากจาก filebase
+      //   console.log('y');
+      //   console.log(snapshot.val());
 
-      }, function (error) {
-        console.log("Error: " + error.code);
-      })
+      // }, function (error) {
+      //   console.log("Error: " + error.code);
+      // })
 
       // var write_Ref = ref("a"); // แก้ไข้ข้อมูลใน a ที่ index 0 เป็น 1
       // write_Ref.update ({
