5207852a7b188020ea53e42f17082988bf0e66fa
ffon <poty1900@hotmail.com>
2018-03-30 21:40:56 +0700

    Update test_insert_data.php

--- test_insert_data.php
+++ test_insert_data.php
@@ -38,7 +38,7 @@
 // 	   "0" : 1
 // 	});
 	      
-	var push_Ref = firebase.database().ref("key1").push ("data1"); // เพิ่มข้อมูลลงใน file base
+	var push_Ref = firebase.database().ref("id").push ({"key":"value"}); // เพิ่มข้อมูลลงใน file base
 	
 	     
 	      
