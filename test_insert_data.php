133830af8accd0ec857770dc9f6757da50f225d0
ffon <poty1900@hotmail.com>
2018-03-30 18:37:49 +0700

    Update test_insert_data.php

--- test_insert_data.php
+++ test_insert_data.php
@@ -1,12 +1,13 @@
 <html>  
-    <head>  1
+    <head>  test
 	<title>Save to Data file using PHP</title>  
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script></head>  
     <body>  
-	    <script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
       <script>
+	      console.log('y');
         // Initialize Firebase
         var config = {
           apiKey: "AIzaSyDd1JiN7FNLo9KvG9OCe1Mrn-ZB_9_bNFE",
