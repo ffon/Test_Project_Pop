83e1a11dda23115acbe97ce383e72dff8a990b8d
ffon <poty1900@hotmail.com>
2018-03-30 19:05:43 +0700

    Update test_insert_data.php

--- test_insert_data.php
+++ test_insert_data.php
@@ -7,7 +7,6 @@
     <body>  
 <script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
       <script>
-	      console.log('y');
         // Initialize Firebase
         var config = {
           apiKey: "AIzaSyDd1JiN7FNLo9KvG9OCe1Mrn-ZB_9_bNFE",
@@ -18,6 +17,24 @@
           messagingSenderId: "421898172154"
         };
         firebase.initializeApp(config);
+	      
+	  // ------  
+	  var rootRef = firebase.database().ref();
+          var storesRef = rootRef.child('Lorelle/Visitors');
+          var newStoreRef = storesRef.push();
+          newStoreRef.set($scope.CarModal,
+            function(error) {
+              //NOTE: this completion has a bug, I need to fix.
+              if (error) {
+                   console.log("Data could not be saved." + error);
+                   Materialize.toast('Error: Failed to Submit' + error, 2000);
+              } else {
+                 console.log("Data saved successfully.");
+                 Materialize.toast('Data Submitted, Thank You.', 2000);
+
+                   }
+                });
+             }
       </script>
 	
     </body>  
